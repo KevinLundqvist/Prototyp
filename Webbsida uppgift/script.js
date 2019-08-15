e3a23aee53ad94ea6951e63b0cd05b55525a079f
KevinLundqvist <kevin-erik1@hotmail.comm>
2019-08-15 14:16:06 +0300

    Initial commit

---
+++ Webbsida uppgift/script.js
@@ -0,0 +1,9 @@
+let input = document.getElementById("name");
+let button = document.getElementById("button");
+
+button.addEventListener('click', onButtonClick);
+
+
+function onButtonClick() {
+    alert("Hello " + input.value);
+}
