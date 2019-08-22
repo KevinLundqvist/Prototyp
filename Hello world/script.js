490a5e7da793013a508fde0670797455b4008440
KevinLundqvist <kevin-erik1@hotmail.comm>
2019-08-22 10:24:05 +0300

    ny uppgift

---
+++ Hello world/script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("button");
+
+let MyText = document.getElementById("Hello World");
+
+button.addEventListener('click', OnButtonClicked);
+
+function OnButtonClicked() {
+
+    alert("Hello World")
+};
